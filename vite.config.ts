b5bca19bdf40f06908e4614cf3673e96601add1a
Nikita <nik.veras@mail.ru>
2023-08-17 14:53:27 +0300

    зависимости и обновление корня

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/image-converter-serverless/",
+  base: "/",
   plugins: [vue()],
   resolve: {
     alias: {
